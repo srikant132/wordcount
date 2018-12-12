37f35d8bafedc6203020409157500729fe796118
srikant132 <srikantcs132@gmail.com>
2018-12-12 06:00:07 +0530

    this is the complete part of basic django project word count

--- wordcount/urls.py
+++ wordcount/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path('',views.home),
-        path('count/',views.count,name='count')
+        path('',views.home,name='home'),
+        path('count/',views.count,name='count'),
+        path('about/',views.about,name='about')
 ]
--- wordcount/views.py
+++ wordcount/views.py
@@ -1,8 +1,29 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import operator
 
 def home(request):
     return render(request,'home.html',{})
 
+def about(request):
+    return render(request,'about.html')
+    
 def count(request):
-    return render(request,'count.html')    
+    fulltext = request.GET['fulltext']
+    wordlist=fulltext.split()
+
+    #create worddictionary
+    worddictionary = {}
+
+    for word in wordlist:
+        if word in worddictionary:
+            #increase the number
+             worddictionary[word] +=1
+        else:
+            #add to the worddictionary
+            worddictionary[word] = 1
+
+    sortedwords = sorted(worddictionary.items(),key=operator.itemgetter(1),reverse=True)
+
+
+    return render(request,'count.html',{'fulltext':fulltext,'count':len(wordlist),'sortedwords': sortedwords})
